481364cad04196720fec560cd0acaf4fed352ea6
John SJ Anderson <genehack@genehack.org>
2010-05-01 11:21:28 -0400

    pod

--- lib/MooseX/Role/Pluggable.pm
+++ lib/MooseX/Role/Pluggable.pm
@@ -76,3 +76,83 @@ sub _map_plugins_to_libs {
 
 no Moose::Role;
 1;
+
+__END__
+
+=head1 NAME
+
+MooseX::Role::Pluggable - add plugins to your Moose classes
+
+=head1 SYNOPSIS
+
+    package MyMoose;
+    use Moose;
+    with 'MooseX::Role::Pluggable';
+
+    my $moose = MyMoose->new({
+      plugins => [ 'Antlers' , 'Tail' , '+After::Market::GroundEffectsPackage' ] ,
+      # other args here
+    });
+
+    foreach my $plugin ( @{ $moose->plugin_list } ) {
+      if ( $plugin->can( 'some_method' )) {
+        $plugin->some_method();
+      }
+    }
+
+    # call a method in a particular plugin directly
+    # (plugin_hash() returns a hash ref of 'plugin_name => plugin')
+    $moose->plugin_hash->{Antlers}->gore( $other_moose );
+
+    # plugins are indexed by the name that was used in the original 'plugins' list
+    $moose->plugin_hash->{After::Market::GroundEffectsPackage}->light_up();
+
+    # see the documentation for MooseX::Role::Pluggable::Plugin for info on
+    # how to write plugins...
+
+=head1 DESCRIPTION
+
+This is a role that allows your class to consume an arbitrary set of plugin
+modules and then access those plugins and use them to do stuff.
+
+Plugins are loaded based on the list of plugin names in the 'plugins'
+attribute. Names that start with a '+' are used as the full name to load;
+names that don't start with a leading '+' are assumed to be in a 'Plugins'
+namespace under your class name. (E.g., if your app is 'MyApp', plugins will
+be loaded from 'MyApp::Plugin').
+
+NOTE: Plugins are lazily loaded -- that is, no plugins will be loaded until
+either the 'plugin_list' or 'plugin_hash' methods are called. If you want to
+force plugins to load at object instantiation time, your best bet is to call
+one of those method right after you call 'new()'.
+
+Plugin classes should consume the 'MooseX::Role::Pluggable::Plugin' role; see
+the documentation for that module for more information.
+
+=head1 METHODS
+
+=head2 plugin_hash
+
+Returns a hashref with a mapping of 'plugin_name' to the actual plugin object.
+
+=head2 plugin_list
+
+Returns an arrayref of loaded plugin objects.
+
+=head1 AUTHOR
+
+John SJ Anderson, C<genehack@genehack.org>
+
+=head1 SEE ALSO
+
+L<MooseX::Role::Pluggable::Plugin>, L<MooseX::Object::Pluggable>,
+L<Object::Pluggable>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010, John SJ Anderson
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
--- lib/MooseX/Role/Pluggable/Plugin.pm
+++ lib/MooseX/Role/Pluggable/Plugin.pm
@@ -22,3 +22,50 @@ has parent => (
 no Moose::Role;
 1;
 
+__END__
+
+=head1 NAME
+
+MooseX::Role::Pluggable::Plugin - add plugins to your Moose classes
+
+=head1 SYNOPSIS
+
+    package MyMoose::Plugin::Antlers;
+    use Moose;
+    with 'MooseX::Role::Pluggable::Plugin';
+
+    sub gore {
+      my( $self ) = shift;
+
+      # yadda yadda yadda
+    }
+
+    # see the documentation for MooseX::Role::Pluggable for info on how to get
+    # your Moose class to use this plugin...
+
+=head1 DESCRIPTION
+
+This is a role that must be consumed by any plugin that's going to be used
+with the 'MooseX::Role::Pluggable' role. It serves to make sure that required
+attributes are in place and contain the expected sorts of info.
+
+Classes that want to utilize plugins should consume the
+'MooseX::Role::Pluggable' role; see the documentation for that module for more
+information.
+
+=head1 AUTHOR
+
+John SJ Anderson, C<genehack@genehack.org>
+
+=head1 SEE ALSO
+
+L<MooseX::Role::Pluggable>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010, John SJ Anderson
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
