6fd59d3e6e4e6aed351d0cfa5ba281010aa2bcbb
Jody Clements <clementsj@janelia.hhmi.org>
2013-02-26 10:23:57 -0500

    More updates to the plugin_list documentation
    Updated the README to reflect the changes in the POD.

--- lib/MooseX/Role/Pluggable.pm
+++ lib/MooseX/Role/Pluggable.pm
@@ -150,8 +150,8 @@ Returns a hashref with a mapping of 'plugin_name' to the actual plugin object.
 
 =head2 plugin_list
 
-Returns an arrayref of loaded plugin objects. The array will have the same order
-as the plugins array ref passed in during object creation.
+Returns an arrayref of loaded plugin objects. The arrayref will have the
+same order as the plugins array ref passed in during object creation.
 
 =head1 AUTHOR
 
